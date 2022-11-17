a0b49acb4e4cdfc131eef933efe39d99572fc193
Mohammad Nikravan <madnik7@gmail.com>
2022-11-17 11:18:24 -0800

    change GrayMint exception brand

--- GrayMint.Common.AspNetCore/AppExceptionHandlerExtension.cs
+++ GrayMint.Common.AspNetCore/AppExceptionHandlerExtension.cs
@@ -3,18 +3,26 @@ using System.Net;
 using System.Net.Mime;
 using System.Text.Json;
 using GrayMint.Common.Exceptions;
+using Microsoft.Extensions.Options;
 
 namespace GrayMint.Common.AspNetCore;
 
 public static class AppExceptionExtension
 {
+    public class AppExceptionOptions
+    {
+        public string? RootNamespace { get; set; }
+    }
+
     public class CustomExceptionMiddleware
     {
         private readonly RequestDelegate _next;
+        private readonly AppExceptionOptions _appExceptionOptions;
 
-        public CustomExceptionMiddleware(RequestDelegate next)
+        public CustomExceptionMiddleware(RequestDelegate next, IOptions<AppExceptionOptions> appExceptionOptions)
         {
             _next = next;
+            _appExceptionOptions = appExceptionOptions.Value;
         }
 
         private static Type GetExceptionType(Exception ex)
@@ -38,15 +46,22 @@ public static class AppExceptionExtension
                 else if (ex is UnauthorizedAccessException) context.Response.StatusCode = (int)HttpStatusCode.Forbidden;
                 else context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
 
+                // create typeFullName
+                var typeFullName = GetExceptionType(ex).FullName;
+                if (!string.IsNullOrEmpty(_appExceptionOptions.RootNamespace))
+                    typeFullName = typeFullName?.Replace(nameof(GrayMint), _appExceptionOptions.RootNamespace);
+
                 // set optional information
                 context.Response.ContentType = MediaTypeNames.Application.Json;
                 var error = new
                 {
                     Data = new Dictionary<string, string?>(),
                     TypeName = GetExceptionType(ex).Name,
-                    TypeFullName = GetExceptionType(ex).FullName,
+                    TypeFullName = typeFullName,
                     ex.Message
                 };
+
+
 
                 foreach (DictionaryEntry item in ex.Data)
                 {
@@ -59,9 +74,10 @@ public static class AppExceptionExtension
         }
     }
 
-    public static IApplicationBuilder UseAppExceptionHandler(this IApplicationBuilder app)
+    public static IApplicationBuilder UseAppExceptionHandler(this IApplicationBuilder app, AppExceptionOptions? appExceptionOptions = null)
     {
-        app.UseMiddleware<CustomExceptionMiddleware>();
+        appExceptionOptions ??= new AppExceptionOptions();
+        app.UseMiddleware<CustomExceptionMiddleware>(Options.Create(appExceptionOptions));
         return app;
     }
 }
