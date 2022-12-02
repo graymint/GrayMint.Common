5f4494377edf99b46d61eef120ff80b6a434db1a
madnik7 <madnik7@gmail.com>
2022-12-02 13:18:42 -0800

    format code

--- GrayMint.Common.Test/recreate-api.ps1
+++ GrayMint.Common.Test/recreate-api.ps1
@@ -1,10 +1,12 @@
 $curDir = $PSScriptRoot;
 $solutionDir = (Split-Path $PSScriptRoot -Parent);
+
+# variables
 $projectFile="$solutionDir\GrayMint.Common.Test.WebApiSample\GrayMint.Common.Test.WebApiSample.csproj";
 $namespace = "GrayMint.Common.Test.Api";
+$nswagFile = "$curDir/Api/Api.nswag";
 
-$nswag = "${Env:ProgramFiles(x86)}\Rico Suter\NSwagStudio\Net70\dotnet-nswag.exe";
+# run
+$nswagExe = "${Env:ProgramFiles(x86)}\Rico Suter\NSwagStudio\Net70\dotnet-nswag.exe";
 $variables="/variables:namespace=$namespace,apiFile=Api.cs,projectFile=$projectFile";
-
-
-& "$nswag" run "$curDir/Api/Api.nswag" $variables /runtime:Net70;
+& "$nswagExe" run $nswagFile $variables /runtime:Net70;
