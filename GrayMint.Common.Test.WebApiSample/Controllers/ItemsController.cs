cb7a6d72d7761bd6f8e92a163ded3cbaf4665c8d
madnik7 <madnik7@gmail.com>
2023-03-22 00:16:08 -0700

    add more roles to ItemController

--- GrayMint.Common.Test.WebApiSample/Controllers/ItemsController.cs
+++ GrayMint.Common.Test.WebApiSample/Controllers/ItemsController.cs
@@ -20,7 +20,7 @@ public class ItemsController : ControllerBase
     }
 
     [HttpPost]
-    [Authorize(SimpleRoleAuth.Policy, Roles = nameof(Roles.AppUser))]
+    [Authorize(SimpleRoleAuth.Policy, Roles = $"{nameof(Roles.AppUser)},{nameof(Roles.AppReader)}")]
     public async Task<Item> Create(int appId, string itemName)
     {
         var ret = await _dbContext.Items.AddAsync(new Item { AppId = appId, ItemName = itemName });
@@ -38,7 +38,7 @@ public class ItemsController : ControllerBase
     }
 
     [HttpGet("itemId")]
-    [Authorize(SimpleRoleAuth.Policy, Roles = nameof(Roles.AppReader))]
+    [Authorize(SimpleRoleAuth.Policy, Roles = $"{nameof(Roles.AppUser)},{nameof(Roles.AppReader)}")]
     public async Task<Item> Get(int appId, int itemId)
     {
         var ret = await _dbContext.Items.SingleAsync(x => x.AppId == appId && x.ItemId == itemId);
