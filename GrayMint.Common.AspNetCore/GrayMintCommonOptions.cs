112bd5cde4afb572180fd8738d2e4d18abd21929
madnik7 <madnik7@gmail.com>
2023-11-05 01:02:17 -0700

    remove appName from options

--- GrayMint.Common.AspNetCore/GrayMintCommonOptions.cs
+++ GrayMint.Common.AspNetCore/GrayMintCommonOptions.cs
@@ -2,12 +2,7 @@ namespace GrayMint.Common.AspNetCore;
 
 public class GrayMintCommonOptions
 {
-    public string AppName { get; set; } = default!;
-
     internal void Validate()
     {
-        // Configure Settings
-        if (string.IsNullOrWhiteSpace(AppName))
-            GrayMintApp.ThrowOptionsValidationException(nameof(AppName), typeof(string));
     }
 }
