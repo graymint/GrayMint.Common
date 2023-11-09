daa1be532ea9ba932ea7378c27813cf42c5fde19
madnik7 <madnik7@gmail.com>
2023-11-08 20:11:05 -0800

    fix AssertApiException

--- GrayMint.Common/Utils/TestUtil.cs
+++ GrayMint.Common/Utils/TestUtil.cs
@@ -89,7 +89,7 @@ public static  class TestUtil
         catch (ApiException ex)
         {
             if (ex.ExceptionTypeName != expectedExceptionType)
-                throw new Exception($"Expected {expectedExceptionType} but was {ex.GetType().Name}. {message}");
+                throw new Exception($"Expected {expectedExceptionType} but was {ex.ExceptionTypeName}. {message}");
         }
     }
 
